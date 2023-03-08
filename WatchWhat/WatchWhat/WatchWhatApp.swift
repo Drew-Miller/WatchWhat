093fd55a1b7c98a8ef50a4d3051cdd5bd71f0e16
Drew Miller <d.m.lespaul@gmail.com>
2023-03-08 11:10:22 -0800

    initial create

---
+++ WatchWhat/WatchWhat/WatchWhatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWhatApp.swift
+//  WatchWhat
+//
+//  Created by Drew Miller on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchWhatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
